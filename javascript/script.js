1d13aa72943a9a7818cf276a672cc5008dfb2064
amarpustam45 <amarpustam@hotmail.com>
2021-10-23 16:07:18 -0400

    working canvas

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,59 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
+let isDrawing = false;
+let mouseX = null;
+let mouseY = null;
 
-windowSize();
 
-function windowSize(){
-    ctx.canvas.width = window.innerWidth;
-    // ctx.canvas.height = window.innerHeight; 
+window.onload = () => {
+    ctx.canvas.width = window.innerWidth - 40;
+    ctx.canvas.height = window.innerHeight - 128;
+    console.log(ctx); 
+    clear();
 }
 
-canvas.addEventListener("click", () => {
-    draw();
+window.addEventListener("resize", () => {
+    ctx.canvas.width = window.innerWidth - 40;
+    ctx.canvas.height = window.innerHeight - 128; 
+    clear();
 })
 
-function draw(){
+function clear(){
     ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.arc(100,100,20,0,Math.PI*2);
-    ctx.fill();
+    ctx.fillStyle = "white";
+    ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fill();    
+}
+
+canvas.addEventListener("mousedown", (e) => {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+    isDrawing =  true;
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing === true){
+        draw(mouseX, mouseY, e.offsetX, e.offsetY);
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    }
+})
+
+canvas.addEventListener("mouseup", (e) => {
+    if (isDrawing === true){
+        draw(mouseX, mouseY, e.offsetX, e.offsetY);
+        mouseX = null;
+        mouseY = null;
+        isDrawing = false;
+    }
+})
+
+function draw(x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWith = 3;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
 }
