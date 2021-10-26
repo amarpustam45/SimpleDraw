848a44de5e02948fcd610dffea5b6b8b604ea91a
amarpustam45 <amarpustam@hotmail.com>
2021-10-26 15:01:19 -0400

    working undo

--- javascript/script.js
+++ javascript/script.js
@@ -5,26 +5,69 @@ let mouseX = null;
 let mouseY = null;
 let colour = "black";
 let pen__size = 10;
+let undo_array = [];
+let index = -1;
 
 window.onload = () => {
     ctx.canvas.width = window.innerWidth - 40;
     ctx.canvas.height = window.innerHeight - 128;
-    console.log(ctx); 
-    clear();
+    ctx.fillStyle = "white";
+    ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fill();
 }
 
 window.addEventListener("resize", () => {
     ctx.canvas.width = window.innerWidth - 40;
     ctx.canvas.height = window.innerHeight - 128; 
-    clear();
 })
 
-function clear(){
-    ctx.beginPath();
+function clear_screen(){
     ctx.fillStyle = "white";
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fill();   
+    ctx.fill();
+
+    undo_array = [];
+    index = -1;
 }
+
+
+// canvas.addEventListener("touchstart", start, false);
+// canvas.addEventListener("mousedown", start, false);
+// canvas.addEventListener("touchmove", draw_line, false);
+// canvas.addEventListener("mousemove", draw_line, false);
+// canvas.addEventListener("touchend", stop_draw, false);
+// canvas.addEventListener("mouseup", stop_draw, false);
+// canvas.addEventListener("mouseout", stop_draw, false);
+
+
+// function start(e){
+//     mouseX = e.offsetX;
+//     mouseY = e.offsetY;
+//     isDrawing =  true;
+// }
+
+// function draw_line(e){
+//     if (isDrawing === true){
+//         draw(mouseX, mouseY, e.offsetX, e.offsetY);
+//         mouseX = e.offsetX;
+//         mouseY = e.offsetY;
+//     }
+// }
+
+// function stop_draw(e){
+//     if (isDrawing === true){
+//         draw(mouseX, mouseY, e.offsetX, e.offsetY);
+//         mouseX = null;
+//         mouseY = null;
+//         isDrawing = false;
+//     }
+
+//     undo_array.push(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
+//     index += 1;
+//     console.log(undo_array);
+//     console.log(index);
+// }
 
 canvas.addEventListener("mousedown", (e) => {
     mouseX = e.offsetX;
@@ -47,6 +90,11 @@ canvas.addEventListener("mouseup", (e) => {
         mouseY = null;
         isDrawing = false;
     }
+
+    undo_array.push(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
+    index += 1;
+    console.log(undo_array);
+    console.log(index);
 })
 
 function draw(x1, y1, x2, y2){
@@ -58,4 +106,14 @@ function draw(x1, y1, x2, y2){
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
+}
+
+function undo_function(){
+    if(index <= 0){
+        clear_screen();
+    } else {
+        index -= 1;
+        undo_array.pop();
+        ctx.putImageData(undo_array[index], 0, 0);
+    }
 }
