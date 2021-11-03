f8cec548894e5081f777fa4eac8a9e52ac906ad2
amarpustam45 <amarpustam@hotmail.com>
2021-11-03 14:32:01 -0400

    hopefully works now

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ let index = -1;
 const rect = canvas.getBoundingClientRect();
 
 window.onload = () => {
-    ctx.canvas.width = window.innerWidth - 40;
+    ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight - 160;
     clear_screen();
 }
@@ -30,44 +30,6 @@ function clear_screen(){
     undo_array = [];
     index = -1;
 }
-
-
-// canvas.addEventListener("touchstart", start, false);
-// canvas.addEventListener("mousedown", start, false);
-// canvas.addEventListener("touchmove", draw_line, false);
-// canvas.addEventListener("mousemove", draw_line, false);
-// canvas.addEventListener("touchend", stop_draw, false);
-// canvas.addEventListener("mouseup", stop_draw, false);
-// canvas.addEventListener("mouseout", stop_draw, false);
-
-
-// function start(e){
-//     mouseX = e.offsetX;
-//     mouseY = e.offsetY;
-//     isDrawing =  true;
-// }
-
-// function draw_line(e){
-//     if (isDrawing === true){
-//         draw(mouseX, mouseY, e.offsetX, e.offsetY);
-//         mouseX = e.offsetX;
-//         mouseY = e.offsetY;
-//     }
-// }
-
-// function stop_draw(e){
-//     if (isDrawing === true){
-//         draw(mouseX, mouseY, e.offsetX, e.offsetY);
-//         mouseX = null;
-//         mouseY = null;
-//         isDrawing = false;
-//     }
-
-//     undo_array.push(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
-//     index += 1;
-//     console.log(undo_array);
-//     console.log(index);
-// }
 
 canvas.addEventListener("touchstart", (e) => {
     mouseX = e.touches[0].clientX - rect.left;
